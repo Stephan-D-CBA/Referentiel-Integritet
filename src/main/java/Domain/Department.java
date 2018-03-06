86e1657fd8a4847d05cdc1d4187659b7bd96d353
super <super@DESKTOP-4SJ2N20>
2018-03-06 12:27:33 +0100

    Department

--- src/main/java/Domain/Department.java
+++ src/main/java/Domain/Department.java
@@ -1,3 +1,7 @@
+package Domain;
+
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +13,40 @@
  * @author Stephan
  */
 public class Department {
-    
+    private int id;
+    private String name;
+    private int manager;
+    private List<Employee> employees;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getManager() {
+        return manager;
+    }
+
+    public void setManager(int manager) {
+        this.manager = manager;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
 }
