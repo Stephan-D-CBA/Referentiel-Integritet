a34ce5777df2dd023478a93f662e95192c422fa5
Stephan-D-CBA <stephan.mdd@gmail.com>
2018-03-06 12:31:02 +0100

    Employee

--- src/main/java/Domain/Employee.java
+++ src/main/java/Domain/Employee.java
@@ -1,13 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package Domain;
 
-/**
- *
- * @author Stephan
- */
+import java.util.List;
+
 public class Employee {
     private int id;
+    private String name;
+    private String title;
+    private List<Department> departments;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Department> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(List<Department> departments) {
+        this.departments = departments;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", name=" + name + ", title=" + title + ", departments=" + departments + '}';
+    }    
 }
